354ebca608728cd358e6697dc2d69c82ce29d106
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-06-07 14:35:58 -0400

    before second news api

--- src/containers/artists_list.js
+++ src/containers/artists_list.js
@@ -1,29 +1,30 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
- class ArtistList extends Component {
-   renderArtist(response) {
+class ArtistList extends Component {
+  renderArtist(response) {
 
-
-     let results = response.topartists;
-     if (!results.artist) return <div>Error</div>;
-     results = results.artist // Error handling for if there is no artists.
-     if (results.length > 5) results.length = 5;
-     console.log(results);
-     return (
+    let results = response.topartists;
+    if (!results.artist)
+      return <div>Error</div>;
+    results = results.artist // Error handling for if there is no artists.
+    if (results.length > 5)
+      results.length = 5;
+    console.log(results);
+    return (
       <div className="artists" key='hi'>
-           {results.map(function(result, index){
-                  return <div className='artist' key={ index }>
-                    <img className='artist-image' src={result.image[3]['#text']}/>
-                    <a className='artist-name' href={result.url}>{result.name}</a>
-                  </div>;
-                  })}
-     </div>
-   )
-   }
+        {results.map(function(result, index) {
+          return <div className='artist' key={index}>
+            <img className='artist-image' src={result.image[3]['#text']}/>
+            <a className='artist-name' href={result.url}>{result.name}</a>
+          </div>;
+        })}
+      </div>
+    )
+  }
 
-   render(){
-    return(
+  render() {
+    return (
       <div className="artist-container">
         <h1>Top Artists</h1>
         <div>{this.props.artist.map(this.renderArtist)}</div>
--- src/containers/news_list.js
+++ src/containers/news_list.js
@@ -2,20 +2,23 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class NewsList extends Component {
-   renderNews(response) {
+  renderNews(response) {
     const results = response.response.results;
 
     return (
       <div key='hi'>
-          {results.map(function(result, index){
-        return <h6 key={ index }><a href={result.webUrl}>{result.webTitle}</a></h6>;
-      })}
+        {results.map(function(result, index) {
+          return <h6 key={index}>
+            <a href={result.webUrl}>{result.webTitle}</a>
+          </h6>;
+          
+        })}
       </div>
     )
-      }
+  }
 
-   render(){
-    return(
+  render() {
+    return (
       <div className="news-container">
         <h1>News</h1>
         {this.props.news.map(this.renderNews)}
