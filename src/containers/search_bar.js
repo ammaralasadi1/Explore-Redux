f22a9413746375757eb43b2e8ba5a4e8d368ceef
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-06-15 02:17:15 -0400

    before deploy 2

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      term: 'America'
+      term: 'France'
     }
     //the initial state
     this.OninputChange = this.OninputChange.bind(this);
